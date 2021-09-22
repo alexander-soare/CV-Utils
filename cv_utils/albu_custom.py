40d19d3aeb3ab96f744a98b7d571b9cf79512277
Alexander Soare <alexander.soare159@gmail.com>
2021-09-22 10:43:47 +0100

    Add random scale augmentation

--- cv_utils/albu_custom.py
+++ cv_utils/albu_custom.py
@@ -3,8 +3,12 @@ Just my custom albumentations addons
 NOTE TO SELF: Be careful with these as you've been bitten by albumentations code changes before!
 """
 
+from typing import Tuple, Union
+import random
+
 from albumentations.core.transforms_interface import ImageOnlyTransform
 import albumentations.augmentations.functional as F
+import cv2
 
 
 class Grid(ImageOnlyTransform):
@@ -23,7 +27,7 @@ class Grid(ImageOnlyTransform):
 
 class ResizeWidth(ImageOnlyTransform):
     """
-    resize by specifying only target width. Can lock aspect ratio if desired
+    Resize by specifying only target width. Can lock aspect ratio if desired
     """
     def __init__(self, width, lock_aspect_ratio=True):
         super().__init__(True, 1)
@@ -35,4 +39,43 @@ class ResizeWidth(ImageOnlyTransform):
         if self.lock_ar:
             new_height = int(round(self.width/width*new_height))
         img = F.resize(img, new_height, self.width)
+        return img
+
+
+class RandomScale(ImageOnlyTransform):
+    """
+    Like Albumentations random scale but we have control over height and width
+    separately
+    Output image is different size from input image
+    """
+    def __init__(self, height_scale_limit: Union[float, Tuple[float, float]],
+                 width_scale_limit: Union[float, Tuple[float, float]],
+                 interpolation=cv2.INTER_LINEAR, p: float = 0.5):
+        """
+        Args:
+            height_scale_limit - Scaling factor range. If it is a single float
+                value, the range will be (1 - height_scale_limit,
+                1 + height_scale_limit). If it is a tuple of two float values,
+                the range will be (height_scale_limit[0], height_scale_limit[1])
+            width_scale_limit - Same concept as height_scale_limit but for
+                width
+            interpolation - cv2 interpolation flag.
+            p - probability of applying the transform
+        """
+        super().__init__(True, 1)
+        if isinstance(height_scale_limit, float):
+            height_scale_limit = (1 - height_scale_limit, 1 + height_scale_limit)
+        self.height_scale_limit = height_scale_limit
+        if isinstance(width_scale_limit, float):
+            width_scale_limit = (1 - width_scale_limit, 1 + width_scale_limit)
+        self.width_scale_limit = width_scale_limit
+        self.interpolation = interpolation
+
+    def apply(self, img, **_):
+        if random.random() > self.p:
+            return img
+        img = cv2.resize(
+            img, (0, 0), fx=random.uniform(*self.width_scale_limit),
+            fy=random.uniform(*self.height_scale_limit),
+            interpolation=self.interpolation)
         return img
